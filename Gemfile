ef388c80300a60e5b2b40249a24205e6c9c21038
dalton <adam.dalton@gmail.com>
2010-11-01 20:52:22 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'jdbc-sqlite3'
+gem 'activerecord-jdbc-adapter'
+gem 'activerecord-jdbcsqlite3-adapter'
+gem 'jruby-openssl'
+gem 'jruby-rack'
+gem 'warbler'
+gem 'neo4j', '1.0.0.beta.14'
+
+
+group :development do
+  gem 'rspec-rails', '2.0.1'
+end
+
+group :test do
+  gem 'rspec', '2.0.1'
+  gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iopenforum::Application.config.secret_token = '652521f95e5b2adb77dddf574b3cd23275451c6e57db459d86f91de165be57b38391eb5c2f4504c2827e90a514358752902412d3f14979721e7e491a8f9b5d08'
